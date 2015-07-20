9cab1c4a45021e9cae9a2e12cb0de20890b18638
zhu <13716912315@163.com>
2015-07-20 15:50:35 +0800

    no

--- src/com/example/searchlayout/MainActivity.java
+++ src/com/example/searchlayout/MainActivity.java
@@ -16,6 +16,17 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		System.out.println(123123);
+		System.out.println(123123);
+		System.out.println(123123);
+		System.out.println(123123);
+		System.out.println(123123);
+		System.out.println(123123);
+		
+		System.out.println(123123);
+		
+		System.out.println(123123);
+		
 		return true;
 	}
 

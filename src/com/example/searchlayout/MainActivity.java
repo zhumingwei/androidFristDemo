4cc8075baf3f8145c494e9e4e47ce004ee02428c
zhu <13716912315@163.com>
2015-07-20 15:52:25 +0800

    ok

--- src/com/example/searchlayout/MainActivity.java
+++ src/com/example/searchlayout/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.searchlayout;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Button b=new Button(this);
 	}
 
 	@Override
